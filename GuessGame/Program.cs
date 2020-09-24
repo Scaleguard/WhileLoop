56e89ceb634dc353cded4c00c8c9222d34ecf1fe
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:33:05 +0300

    While Loop

---
+++ GuessGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int userGuess = Int32.Parse(Console.ReadLine());
+            bool correctGuess = false;
+
+            while (!correctGuess)
+            {
+                if(randomNumber == userGuess)
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+                }else
+                {
+                    Console.WriteLine("What is my number?");
+                    userGuess = Int32.Parse(Console.ReadLine());
+                    if (randomNumber < userGuess )
+                    {
+                        Console.WriteLine("Your guess is bigger than my number");23
+                    } if (randomNumber > userGuess)
+                    {
+                        Console.WriteLine("Your guess is smaller than my number");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int myRandomNumber = rnd.Next(1, 11);
+            //Console.Write($"Your number is {myRandomNumber}");
+            Console.WriteLine("What is my number from 1 to 10: ");
+            int userGuess = Int32.Parse(Console.ReadLine());
+
+            if(myRandomNumber == userGuess)
+            {
+                Console.WriteLine("Congratulations! You've played me!");
+            }else
+            {
+                Console.WriteLine("Heheehehehheheeheh, you failed! Try again ya wee bastard!");
+            }
+        }
+    }
+}
